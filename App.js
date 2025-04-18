311cc33917e4b1ab4ca60b9580eefe6150f6e1cf
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2025-04-19 01:58:12 +0300

    Frontend

---
+++ App.js
@@ -0,0 +1,20 @@
+let yas = 20;
+let para = 3500;
+let sagliklimi = true;
+
+console.log(yas>18 && para>3000 || sagliklimi == true);
+
+let deneme = {
+    yas: 20,
+    para: 3500,
+    sagliklimi: true    
+}
+
+console.log(deneme)
+
+function method1(){
+    let sayi = 10;
+    console.log(sayi);
+}
+
+method1();
