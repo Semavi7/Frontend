f821b4d6c0f602a56480ae452de4e0524fe4fbb4
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2025-04-20 01:46:28 +0300

    Frontend

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-let yas = 20;
-let para = 3500;
-let sagliklimi = true;
+// let yas = 20;
+// let para = 3500;
+// let sagliklimi = true;
 
-console.log(yas>18 && para>3000 || sagliklimi == true);
+// console.log(yas>18 && para>3000 || sagliklimi == true);
 
-let deneme = {
-    yas: 20,
-    para: 3500,
-    sagliklimi: true    
+// let deneme = {
+//     yas: 20,
+//     para: 3500,
+//     sagliklimi: true    
+// }
+
+// console.log(deneme)
+
+// function method1(){
+//     let sayi = 10;
+//     console.log(sayi);
+// }
+
+// method1();
+
+// let isim = prompt("İsminizi giriniz: ");
+// console.log(isim);
+
+// let sonuc = confirm("Devam etmek istiyor musunuz?");
+// console.log(sonuc);
+
+let yas = Number(prompt("Yaşınızı giriniz: "));
+console.log(typeof yas);
+let para = Number(prompt("Para miktarınızı giriniz: "));
+
+if(yas > 18 && para > 3000){
+    console.log("Hoş geldiniz!");
 }
-
-console.log(deneme)
-
-function method1(){
-    let sayi = 10;
-    console.log(sayi);
+else{
+    console.log("Giriş yapamazsınız!");
 }
-
-method1();
